7848d6f155c4fcd426a915210ec7dd7cfd1f429a
Limkon <130622161+Limkon@users.noreply.github.com>
2023-12-11 13:26:59 +0800

    Update login.js

--- login.js
+++ login.js
@@ -13,7 +13,7 @@ const { Client } = require('ssh2');
       const ssh = new Client();
       await new Promise((resolve, reject) => {
         ssh.on('ready', resolve).on('error', reject).connect({
-          host: 'ssh-mov.alwaysdata.net',
+          host: 'ssh-zote.alwaysdata.net',
           port: 22,
           username,
           password,
